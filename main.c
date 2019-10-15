58e07c50a7400efbd833d7d8fe7e5ff33a7cbcc1
bagi0712 <bagi0712@naver.com>
2019-10-15 15:25:51 +0900

    fourth

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+//정수의 제곱을 계산하기 위한 함수 예제
+#include <stdio.h> 
 int square (int n);
 
 int main(void)
